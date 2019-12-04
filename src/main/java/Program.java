da42b80aeac076e442f07ebd32d8e6a92063555e
Ueli Kunz <kunz@ideadapt.net>
2019-12-04 19:32:55 +0100

    update code and docs to latest

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,14 +1,14 @@
 import com.fasterxml.jackson.core.type.TypeReference;
+import jdk.nashorn.internal.ir.ObjectNode;
 
 import java.io.IOException;
 import java.util.Date;
 
 public class Program {
 
-    public static final String FILE_NAME = "user.ser";
+    private static final String FILE_NAME = "user.ser";
 
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
         User user = new User();
         user.setUsername("theman@hotmail.com");
         user.setRegistrationDate(new Date());
@@ -19,38 +19,36 @@ public class Program {
     }
 
     private static void jacksonXML(User user) {
-        XmlSerializer xmlSerializer = new XmlSerializer();
         try {
+            XmlSerializer xmlSerializer = new XmlSerializer();
             xmlSerializer.serialize(user, FILE_NAME);
-            System.out.println("User Object\n\t".concat(user.toString()).concat("\nserialized to ").concat(FILE_NAME).concat("\n"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            print(user, true);
 
-        try {
             User userFromFile = xmlSerializer.deserialize(FILE_NAME, new TypeReference<User>() {});
-            System.out.println("User Object\n\t".concat(userFromFile.toString()).concat("\nread from file ").concat(FILE_NAME));
+            print(userFromFile, false);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     private static void javaNative(User user) {
-        JavaSerializer ser = new JavaSerializer();
+        JavaSerializer javaSerializer = new JavaSerializer();
         try {
-            ser.serialize(user, FILE_NAME);
-            System.out.println("User Object\n\t".concat(user.toString()).concat("\nserialized to ").concat(FILE_NAME).concat("\n"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            javaSerializer.serialize(user, FILE_NAME);
+            print(user, true);
 
-        try {
-            Object userFromFile = ser.deserialize(FILE_NAME);
-            System.out.println("User Object\n\t".concat(userFromFile.toString()).concat("\nread from file ").concat(FILE_NAME));
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
+            Object userFromFile = javaSerializer.deserialize(FILE_NAME);
+            print(userFromFile, false);
+        } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void print(Object userObject, boolean written){
+        System.out.println("User Object\n\t"
+                .concat(userObject.toString())
+                .concat("\n" + (written ? "written to" : "read from") + " file ")
+                .concat(FILE_NAME)
+                .concat("\n"));
     }
 }
